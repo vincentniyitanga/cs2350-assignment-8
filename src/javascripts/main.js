7b3601fd5c02495948d16f7f90693090f1c3cbd4
vincentniyitanga <vincentniyitanga@mail.weber.edu>
2021-11-20 14:11:18 +0200

    Work done

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,3 +6,36 @@ require.context('../stylesheets/', true, /\.(css)$/i)
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
 
+import { pictures } from './pictures'
+
+for(let p of pictures){
+   let p_thumb = document.getElementById('p' + p.id)
+   p_thumb.innerHTML = `
+      <img src="${p.poster}" alt="${p.title}" class="img-thumbnail"/>
+   
+   `
+   p_thumb.onclick = function(){
+         displayPicture(p)
+   }
+}
+
+let featured_picture = document.querySelector(".featued")
+function displayPicture(picture){
+   featured_picture.innerHTML = `
+   <div class="card" >
+   <div class="card-header">${picture.title}</div>
+   <img src="${picture.poster}" class="card-img-top" alt="${picture.title}">
+   <div class="card-body">
+     <h5 class="card-title"><small>${picture.description}</small></h5>
+     <p class="card-text"></p>
+   </div>
+   <div class="card-footer text-muted">
+     <div class="row row-cols-3">
+         <div class="col"><strong>Title</strong>${picture.title}</div>
+         <div class="col"><strong>Description</strong>${picture.description}</div>
+     </div>
+   </div>
+ </div>
+
+   `
+}
---
+++ src/javascripts/pictures.js
@@ -0,0 +1,77 @@
+export const pictures = [{
+   
+   "id": 1,
+   "title": "At work",
+   "poster": "https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+   "description": "Beatiful work office setup"
+
+   },
+   {
+
+   "id": 2,
+   "title": "Forest",
+   "poster": "https://images.unsplash.com/photo-1637354563042-9a1d26500ff5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+   "description": "In middle of Nowhere"
+   },
+   {
+    "id": 3,
+   "title": "Lonely Night",
+   "poster": "https://images.unsplash.com/photo-1637342672517-47dbb7f83f94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1296&q=60",
+   "description": "Earth, Stars and the Sky" 
+   },
+   {
+      "id": 4,
+      "title": "Water Fall",
+      "poster": "https://images.unsplash.com/photo-1637355055734-0a3e2e49cf0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+      "description": "Let it flow" 
+   },
+   {
+      "id": 5,
+      "title": "Under Water",
+      "poster": "https://images.unsplash.com/photo-1637380781238-9b703b0d2db0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80",
+      "description": "Don't come close" 
+   },
+   {
+      "id": 6,
+      "title": "On the way home",
+      "poster": "https://images.unsplash.com/photo-1637338907996-ac229060ed5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+      "description": "Bye bye" 
+   },
+   {
+      "id": 7,
+      "title": "Yeah it's cold",
+      "poster": "https://images.unsplash.com/photo-1637320116287-afb702acb49f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8&auto=format&fit=crop&w=1296&q=60",
+      "description": "I got to get out of here. It's literally freezing" 
+   },
+   {
+      "id": 8,
+      "title": "Wow, this is beautiful",
+      "poster": "https://images.unsplash.com/photo-1549122752-018ceade3bf6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80",
+      "description": "Sky is green" 
+   },
+   {
+      "id": 9,
+      "title": "Wow, this is beautiful",
+      "poster": "https://images.unsplash.com/photo-1637331684414-1af2c97b7f9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4NHx8fGVufDB8fHx8&auto=format&fit=crop&w=1296&q=60",
+      "description": "Boat" 
+   },
+   {
+      "id": 10,
+      "title": "Up or down",
+      "poster": "https://images.unsplash.com/photo-1637330815260-52a6fb62f23b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+      "description": "Stairs" 
+   },
+   {
+      "id": 11,
+      "title": "Movie loading",
+      "poster": "https://images.unsplash.com/photo-1637330270946-bf899b3ef422?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+      "description": "What are we watching tonight?" 
+   },
+   {
+      "id": 12,
+      "title": "Nobody been here",
+      "poster": "https://images.unsplash.com/photo-1637311859464-5021233b7da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      "description": "For me it's the stars" 
+   }
+    
+]
